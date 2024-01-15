0ac24ec72a763a26260111955634431722ab2bd9
Clint171 <clintsimiyu004@gmail.com>
2024-01-15 16:33:04 +0300

    feat: added calendar access

--- backend/index.js
+++ backend/index.js
@@ -39,40 +39,10 @@ passport.deserializeUser((obj, cb) => {
     cb(null, obj);
 });
 
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email', 'calendar'] }));
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email', 'https://www.googleapis.com/auth/calendar'] }));
 
 app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
     console.log('req.user', req.user);
-    const calendar = google.calendar({
-        version: 'v3',
-        auth: new google.Auth.auth(
-          process.env.GOOGLE_CLIENT_ID,
-          process.env.GOOGLE_CLIENT_SECRET,
-          req.user.accessToken
-        )
-      });
-    calendar.events.list({
-        calendarId: 'primary',
-        timeMin: (new Date()).toISOString(),
-        maxResults: 10,
-        singleEvents: true,
-        orderBy: 'startTime',
-      }, (err, response) => {
-        if (err) {
-          console.log('The API returned an error: ' + err);
-          return;
-        }
-        const events = response.data.items;
-        if (events.length) {
-          console.log('Upcoming 10 events:');
-          events.map((event, i) => {
-            const start = event.start.dateTime || event.start.date;
-            console.log(`${start} - ${event.summary}`);
-          });
-        } else {
-          console.log('No upcoming events found.');
-        }
-      });
     res.redirect('/dashboard');
 });
 
